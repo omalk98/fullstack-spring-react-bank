693c4413ad4d6ddb882588de8cf8b7bd06d2608b
Omar <77292466+omalk98@users.noreply.github.com>
2022-03-26 21:13:38 -0400

    fixed merge conflicts

--- backend/src/main/java/com/bank/backend/transaction/TransactionController.java
+++ backend/src/main/java/com/bank/backend/transaction/TransactionController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.bank.backend.transaction;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,34 +27,3 @@ public class TransactionController {
         transactionService.addNewTransaction(transaction);
     }
 }
-=======
-//package com.bank.backend.transaction;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//
-//@RestController
-//@RequestMapping(path="trans")
-//@CrossOrigin("*")
-//public class TransactionController {
-//
-//    private final TransactionService transactionService;
-//
-//    @Autowired
-//    public TransactionController(TransactionService transactionService) {
-//        this.transactionService = transactionService;
-//    }
-//
-//    @GetMapping
-//    public List<Transaction> getTransactions() {
-//        return transactionService.getTransactions();
-//    }
-//
-//    @PostMapping
-//    public void registerNewTransaction(@RequestBody Transaction transaction) {
-//        transactionService.addNewTransaction(transaction);
-//    }
-//}
->>>>>>> 5cf0f54221d5e4870bc1c29cf2d99317eec7e8b2
